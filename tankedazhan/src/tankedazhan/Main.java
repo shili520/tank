e20efff90d5d6cc9fbcef704f278a39941e842f7
shili520 <60778766+shili520@users.noreply.github.com>
2020-10-25 21:28:04 +0800

    Create Main.java

---
+++ tankedazhan/src/tankedazhan/Main.java
@@ -0,0 +1,18 @@
+package tankedazhan;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+
+		public static void main(String[] args) throws InterruptedException{
+		TankFrame tf = new TankFrame();
+		
+		while(true) {
+			Thread.sleep(50);
+			tf.repaint();
+		}
+	}
+
+}
